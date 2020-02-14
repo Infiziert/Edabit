cf41f714037c4665cc72be60c78a756d3ac0f590
Ole <infiziert.1337@gmail.com>
2020-02-14 19:16:39 +0100

    small change

--- src/java_medium/DecimalPlaces.java
+++ src/java_medium/DecimalPlaces.java
@@ -1,6 +1,7 @@
 package java_medium;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Create a function that returns the number of decimal places a number (given as a string) has.
@@ -18,7 +19,9 @@ public class DecimalPlaces {
      * Return 0 if the number doesn't have any decimal places (see example #2).
      */
     public static int getDecimalPlace(String num) {
-        return num.substring(num.length() / 2 + 1).length();
+        var split_num = num.split(Pattern.quote("."));
+        if (split_num.length < 2) return 0;
+        return split_num[split_num.length - 1].length();
     }
 
 }
