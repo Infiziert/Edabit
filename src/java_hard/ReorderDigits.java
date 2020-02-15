7051c4471e0b356c25a8352e143374acb3df94d6
Ole <infiziert.1337@gmail.com>
2020-02-15 21:44:13 +0100

    update reorder digits challenge

--- src/java_hard/ReorderDigits.java
+++ src/java_hard/ReorderDigits.java
@@ -1,4 +1,13 @@
 package java_hard;
 
 public class ReorderDigits {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] reorderDigits(int[] arr, String orderBy) {
+        return new int[]{};
+    }
+
 }
